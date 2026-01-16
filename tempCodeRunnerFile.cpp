09159e11fe7534a8a3583ddf8275b338571f872a
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-01-16 20:58:49 +0530

    Notes

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -4,12 +4,13 @@ int main(){
     int n;
     cin>>n;
     for (int i=1;i <= n; i++){
+        for(int j=1; j<=n-i;j++){
+        cout<<"  ";
+        }
         for (int j = 1; j <= i; j++){
             cout<< "* ";
         }
-        for(int j=1; j<=n-1;j++){
-        cout<<" ";
-        }
+        
         cout<<endl;
     }
     return 0;
