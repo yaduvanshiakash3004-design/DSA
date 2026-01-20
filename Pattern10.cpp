06d9a8b1e56793d78a83dd75eda3641d179c037c
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-01-20 15:20:20 +0530

    note

---
+++ Pattern10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        // spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        // total pattern
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j % 2 == 0)
+            {
+                //!
+                cout << "! ";
+            }
+
+            else
+            {
+                //*
+                cout << "* ";
+            }
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
--- Pattern9.cpp
+++ Pattern9.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for (int i=0;i <= n; i++){
+    for (int i=n;i >= 1; i--){
          for(int j=1; j<=n-i;j++){
           cout<<"  ";
         }
