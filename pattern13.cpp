c8d95a3dd786d4717f491f9f34c67e7f18ea06c2
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-01-21 14:43:59 +0530

    notes

---
+++ pattern13.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    int n =(m+1)/2;
+    for (int i=1;i <= n; i++){
+        //outer 
+         for(int j=1; j<=n-i;j++){
+          cout<<"  ";
+        }
+          cout<<"* ";
+          //inner space
+          for(int j=1; j<=2*i-3;j++){
+          cout<<"  ";
+
+        }
+        if(i!=1){
+            cout<<"* ";
+        }
+         cout<<endl;
+         
+    }
+      for (int i=n-1;i >= 1; i--){
+        //outer 
+         for(int j=1; j<=n-i;j++){
+          cout<<"  ";
+        }
+          cout<<"* ";
+          //inner space
+          for(int j=1; j<=2*i-3;j++){
+          cout<<"  ";
+
+        }
+        if(i!=1){
+            cout<<"* ";
+        }
+         cout<<endl;
+         
+    }
+
+  
+    return 0;
+}
