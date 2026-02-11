11093cbbcad756e2a0680ae6c04f9cda658b7c4b
yaduvanshiakash3004-design <yaduvanshiakash3004@gmail.com>
2026-02-11 23:15:12 +0530

    Notes

---
+++ Find_value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+// linear search 
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int value=19;
+    for(int i=0;i<n;i++){
+        if(arr[i]==value){
+            cout<<i+1<<endl;
+        }
+    }
+    return 0;
+}
